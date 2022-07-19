eaa37c2df7b61a6e0a786981c46cc7f9b59d131c
Devansh Sirohi <91769500+honey2438@users.noreply.github.com>
2022-07-19 11:04:07 +0530

    Update Pattern_daffodil_company.cpp

--- Pattern_daffodil_company.cpp
+++ Pattern_daffodil_company.cpp
@@ -3,14 +3,14 @@ using namespace std;
 int
 main ()
 {
-  int n, l = 1, x = 2;
+  int n, l = 1, x = 1;
   cout << "Enter the value of n";
   cin >> n;
-  int k = 4 * n;
+  int k = (n-1) * n;
   for (int i = 1; i <= n-1; i++)
     {
       int m = 0;
-      for (int j = 1; j <= 2 * n - 2 * i; j++)
+      for (int j = 1; j <= 2 * n - i-1; j++)
 	{
 	  if (j < i)
 	    {
@@ -18,29 +18,29 @@ main ()
 	    }
 	  else if (j <= n - 1)
 	    {
-	      cout << l<<" ";
+	      cout << l;
 	      l++;
 	    }
-	  else if (j > n - 1 && j <= 2 * n - 2 * i)
+	  else if (j > n - 1)
 	    {
-	      if (k == n * 4)
+	      if (k == n * (n-1))
 		{
 		  k = k - n + i + 1;
 		  m = k;
-		  cout << m<<" ";
+		  cout << m;
 		  m++;
 		}
 	      else if (x != i)
 		{
 		  k = k - n + i;
 		  m = k;
-		  cout << m<<" ";
+		  cout << m;
 		  m++;
 		  x = i;
 		}
 	      else
 		{
-		  cout << m<<" ";
+		  cout << m;
 		  m++;
 		}
 
